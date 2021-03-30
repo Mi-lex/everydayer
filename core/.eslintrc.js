9267dfcd286de1d0194ade9a8d2028e1e055ddee
Mi-lex <mishin-lex@mail.ru>
2021-03-30 22:08:05 +0300

    Add eslint rules

--- core/.eslintrc.js
+++ core/.eslintrc.js
@@ -21,5 +21,21 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': 'error',
+        complexity: ['error', 3],
+        'default-param-last': ['error'],
+        'default-case-last': 'error',
+        'consistent-return': ['error', { treatUndefinedAsUnspecified: false }],
+        'no-constructor-return': 'error',
+        'no-eq-null': 'error',
+        'func-style': ['error', 'expression'],
+        'no-magic-numbers': [
+            'error',
+            {
+                ignore: [1],
+                ignoreArrayIndexes: true,
+                ignoreDefaultValues: true,
+            },
+        ],
     },
 };
