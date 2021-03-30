0deb8a3d09852abd394ad76016cbc457bfe6ed74
Mi-lex <mishin-lex@mail.ru>
2021-03-30 23:46:02 +0300

    Add mongoose into core

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './modules/tasks/tasks.module';
+import env from './config/env';
 
 @Module({
-    imports: [TasksModule],
+    imports: [TasksModule, MongooseModule.forRoot(env.MONGO_URL)],
     controllers: [AppController],
     providers: [AppService],
 })
--- core/src/config/env.ts
+++ core/src/config/env.ts
@@ -3,9 +3,10 @@ import * as envalid from 'envalid';
 
 configDotEnv();
 
-const { port } = envalid;
+const { port, url } = envalid;
 const env = envalid.cleanEnv(process.env, {
     PORT: port({ default: 8000, desc: 'Application port' }),
+    MONGO_URL: url({ default: 'mongodb://localhost:27017/everydayer' }),
 });
 
 export default env;
