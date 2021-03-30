4ce94f2d214ee58a16c52fedd1251ceeda761e68
Mi-lex <mishin-lex@mail.ru>
2021-03-30 22:21:32 +0300

    Fix lintin error

--- core/src/main.ts
+++ core/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import env from './config/env';
 
 // const bootstrap = async () => {
-const bootstrap = async() => {
+const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(
         new ValidationPipe({
